16362432de6dbc00dcc73fc3e4eb4d0a4adf6e9e
xu3woli <prostyliu@gmail.com>
2024-05-20 01:01:26 +0800

    service -1

--- src/views/ServiceView.vue
+++ src/views/ServiceView.vue
@@ -8,110 +8,113 @@
 
 
 <template>
-    <div class="service container">
+    <div class="service">
 
         <section style="margin: 120px 0">
             <h2 class="text-center text-primary-700 text-bold mb-7">服務項目</h2>
-            <div class="row row-gap-4">
-                <div class="col-12 col-sm-6 col-lg-3 ">
-                    <div class="bg-primary-800 p-7 text-center rounded-4">
-                        <img class="mb-4" src="@/assets/images/service-item-visual.svg" alt="visual design">
-                        <h3 class="text-primary-50">平面設計</h3>
+            <div class="container">
+                <div class="row row-gap-4">
+                    <div class="col-12 col-sm-6 col-lg-3 ">
+                        <div class="bg-primary-800 p-7 text-center rounded-4">
+                            <img class="mb-4" src="@/assets/images/service-item-visual.svg" alt="visual design">
+                            <h3 class="text-primary-50">平面設計</h3>
+                        </div>
                     </div>
-                </div>
-                <div class="col-12 col-sm-6 col-lg-3 ">
-                    <div class="bg-primary-800 p-7 text-center rounded-4">
-                        <img class="mb-4" src="@/assets/images/service-item-visual.svg" alt="visual design">
-                        <h3 class="text-primary-50">平面設計</h3>
+                    <div class="col-12 col-sm-6 col-lg-3 ">
+                        <div class="bg-primary-800 p-7 text-center rounded-4">
+                            <img class="mb-4" src="@/assets/images/service-item-visual.svg" alt="visual design">
+                            <h3 class="text-primary-50">平面設計</h3>
+                        </div>
                     </div>
-                </div>
-                <div class="col-12 col-sm-6 col-lg-3 ">
-                    <div class="bg-primary-800 p-7 text-center rounded-4">
-                        <img class="mb-4" src="@/assets/images/service-item-visual.svg" alt="visual design">
-                        <h3 class="text-primary-50">平面設計</h3>
+                    <div class="col-12 col-sm-6 col-lg-3 ">
+                        <div class="bg-primary-800 p-7 text-center rounded-4">
+                            <img class="mb-4" src="@/assets/images/service-item-visual.svg" alt="visual design">
+                            <h3 class="text-primary-50">平面設計</h3>
+                        </div>
                     </div>
-                </div>
-                <div class="col-12 col-sm-6 col-lg-3 ">
-                    <div class="bg-primary-800 p-7 text-center rounded-4">
-                        <img class="mb-4" src="@/assets/images/service-item-visual.svg" alt="visual design">
-                        <h3 class="text-primary-50">平面設計</h3>
+                    <div class="col-12 col-sm-6 col-lg-3 ">
+                        <div class="bg-primary-800 p-7 text-center rounded-4">
+                            <img class="mb-4" src="@/assets/images/service-item-visual.svg" alt="visual design">
+                            <h3 class="text-primary-50">平面設計</h3>
+                        </div>
                     </div>
                 </div>
             </div>
         </section>
-        <section style="margin: 120px 0">
-            <div class="bg-primary-800 text-primary-50" style="padding: 120px 0">
+        <section style="margin: 120px 0" class="bg-primary-800 ">
+            <div class="text-primary-50" style="padding: 120px 0">
                 <h2 class="text-center text-primary-50 text-bold mb-7">服務流程</h2>
-                <div class="d-flex justify-content-center row">
-                    <div class="col-12 col-lg-6">
-                        <div
-                            class="item py-4 ps-6 d-flex align-items-center border border-1 border-primary-500 rounded-1 my-2 mb-10">
-                            <div class="font-tourney display-2 text-primary-500">1</div>
-                            <div class="ms-0 ms-md-6">
-                                <h3 class="text-center text-md-start text-primary-50">初步接洽</h3>
-                                <p class="mb-0 fs-5 text-primary-300">透過表單或其他社群軟體與我聯絡</p>
-                                <p class="mb-4 mb-md-0 fs-5 text-primary-300">根據難易度與複雜度，後續可能需要電話/線上討論</p>
+                <div class="container">
+                    <div class="d-flex justify-content-center row">
+                        <div class="col-12 col-lg-6">
+                            <div
+                                class="align-items-center bg-primary-600 border border-1 border-primary-500 d-flex flex-column flex-sm-row mb-10 my-2 px-6 py-4 rounded-1">
+                                <div class="display-2 font-tourney me-sm-2 text-primary-500">1</div>
+                                <div class="ms-0 ms-md-6">
+                                    <h3 class="text-center text-md-start text-primary-50 text-sm-start">初步接洽</h3>
+                                    <p class="mb-0 fs-5 text-primary-300">透過表單或其他社群軟體與我聯絡</p>
+                                    <p class="mb-4 mb-md-0 fs-5 text-primary-300">根據難易度與複雜度，後續可能需要電話/線上討論</p>
+                                </div>
                             </div>
-                        </div>
-                        <!--  -->
-                        <div
-                            class="item py-4 ps-6 d-flex align-items-center border border-1 border-primary-500 rounded-1 my-2 mb-10">
-                            <div class="font-tourney display-2 text-primary-500">2</div>
-                            <div class="ms-0 ms-md-6">
-                                <h3 class="text-center text-md-start text-primary-50">需求釐清</h3>
-                                <p class="mb-0 fs-5 text-primary-300">透過訪談與問卷，解析客戶需求，並針對痛點給予實際建議</p>
+                            <!--  -->
+                            <div
+                                class="align-items-center bg-primary-600 border border-1 border-primary-500 d-flex flex-column flex-sm-row mb-10 my-2 px-6 py-4 rounded-1">
+                                <div class="display-2 font-tourney me-sm-2 text-primary-500">2</div>
+                                <div class="ms-0 ms-md-6">
+                                    <h3 class="text-center text-md-start text-primary-50 text-sm-start">需求釐清</h3>
+                                    <p class="mb-0 fs-5 text-primary-300">透過訪談與問卷，解析客戶需求，並針對痛點給予實際建議</p>
+                                </div>
                             </div>
-                        </div>
-                        <!--  -->
-                        <div
-                            class="item py-4 ps-6 d-flex align-items-center border border-1 border-primary-500 rounded-1 my-2 mb-10">
-                            <div class="font-tourney display-2 text-primary-500">3</div>
-                            <div class="ms-0 ms-md-6">
-                                <h3 class="text-center text-md-start text-primary-50">委託簽約</h3>
-                                <p class="mb-0 fs-5 text-primary-300">透過表單或其他社群軟體與我聯絡</p>
-                                <p class="mb-4 mb-md-0 fs-5 text-primary-300">根據難易度與複雜度，後續可能需要電話/線上討論</p>
+                            <!--  -->
+                            <div
+                                class="align-items-center bg-primary-600 border border-1 border-primary-500 d-flex flex-column flex-sm-row mb-10 my-2 px-6 py-4 rounded-1">
+                                <div class="display-2 font-tourney me-sm-2 text-primary-500">3</div>
+                                <div class="ms-0 ms-md-6">
+                                    <h3 class="text-center text-md-start text-primary-50 text-sm-start">委託簽約</h3>
+                                    <p class="mb-0 fs-5 text-primary-300">透過表單或其他社群軟體與我聯絡</p>
+                                    <p class="mb-4 mb-md-0 fs-5 text-primary-300">根據難易度與複雜度，後續可能需要電話/線上討論</p>
+                                </div>
                             </div>
-                        </div>
-                        <!--  -->
-                        <div
-                            class="item py-4 ps-6 d-flex align-items-center border border-1 border-primary-500 rounded-1 my-2 mb-10">
-                            <div class="font-tourney display-2 text-primary-500">4</div>
-                            <div class="ms-0 ms-md-6">
-                                <h3 class="text-center text-md-start text-primary-50">版本呈現</h3>
-                                <p class="mb-0 fs-5 text-primary-300">版本呈現</p>
+                            <!--  -->
+                            <div
+                                class="align-items-center bg-primary-600 border border-1 border-primary-500 d-flex flex-column flex-sm-row mb-10 my-2 px-6 py-4 rounded-1">
+                                <div class="display-2 font-tourney me-sm-2 text-primary-500">4</div>
+                                <div class="ms-0 ms-md-6">
+                                    <h3 class="text-center text-md-start text-primary-50 text-sm-start">版本呈現</h3>
+                                    <p class="mb-0 fs-5 text-primary-300">根據合約內容進行設計或開發</p>
+                                </div>
                             </div>
-                        </div>
-                        <!--  -->
-                        <div
-                            class="item py-4 ps-6 d-flex align-items-center border border-1 border-primary-500 rounded-1 my-2 mb-10">
-                            <div class="font-tourney display-2 text-primary-500">5</div>
-                            <div class="ms-0 ms-md-6">
-                                <h3 class="text-center text-md-start text-primary-50">來回修正</h3>
-                                <p class="mb-0 fs-5 text-primary-300">依據合約規定之修正次數進行，次數計算以 mail 為準</p>
+                            <!--  -->
+                            <div
+                                class="align-items-center bg-primary-600 border border-1 border-primary-500 d-flex flex-column flex-sm-row mb-10 my-2 px-6 py-4 rounded-1">
+                                <div class="display-2 font-tourney me-sm-2 text-primary-500">5</div>
+                                <div class="ms-0 ms-md-6">
+                                    <h3 class="text-center text-md-start text-primary-50 text-sm-start">來回修正</h3>
+                                    <p class="mb-0 fs-5 text-primary-300">依據合約規定之修正次數進行，次數計算以 mail 為準</p>
+                                </div>
                             </div>
-                        </div>
-                        <!--  -->
-                        <div
-                            class="item py-4 ps-6 d-flex align-items-center border border-1 border-primary-500 rounded-1 my-2 mb-10">
-                            <div class="font-tourney display-2 text-primary-500">6</div>
-                            <div class="ms-0 ms-md-6">
-                                <h3 class="text-center text-md-start text-primary-50">終版確認</h3>
-                                <p class="mb-0 fs-5 text-primary-300">雙方同意後即為最終版</p>
+                            <!--  -->
+                            <div
+                                class="align-items-center bg-primary-600 border border-1 border-primary-500 d-flex flex-column flex-sm-row mb-10 my-2 px-6 py-4 rounded-1">
+                                <div class="display-2 font-tourney me-sm-2 text-primary-500">6</div>
+                                <div class="ms-0 ms-md-6">
+                                    <h3 class="text-center text-md-start text-primary-50 text-sm-start">終版確認</h3>
+                                    <p class="mb-0 fs-5 text-primary-300">雙方同意後即為最終版</p>
+                                </div>
                             </div>
-                        </div>
-                        <!--  -->
-                        <div
-                            class="item py-4 ps-6 d-flex align-items-center border border-1 border-primary-500 rounded-1 my-2">
-                            <div class="font-tourney display-2 text-primary-500">7</div>
-                            <div class="ms-0 ms-md-6">
-                                <h3 class="text-center text-md-start text-primary-50">結案</h3>
-                                <p class="mb-0 fs-5 text-primary-300">客戶簽署驗收單，並於約定日期支付尾款</p>
+                            <!--  -->
+                            <div
+                                class="align-items-center bg-primary-600 border border-1 border-primary-500 d-flex flex-column flex-sm-row mb-10 my-2 px-6 py-4 rounded-1">
+                                <div class="display-2 font-tourney me-sm-2 text-primary-500">7</div>
+                                <div class="ms-0 ms-md-6">
+                                    <h3 class="text-center text-md-start text-primary-50 text-sm-start">結案</h3>
+                                    <p class="mb-0 fs-5 text-primary-300">客戶簽署驗收單，並於約定日期支付尾款</p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
         </section>
         <section style="margin: 120px 0">
             <h2>價格表</h2>
